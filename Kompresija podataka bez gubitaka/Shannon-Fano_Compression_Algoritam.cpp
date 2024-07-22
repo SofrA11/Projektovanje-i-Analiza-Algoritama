8d54df6a349f48e2a48827430ce43635ff4e1e68
Mihajlo Sofronijević <mihajlo.sofronijevic@elfak.rs>
2024-07-22 14:57:15 +0200

    vizualizator

--- Kompresija podataka bez gubitaka/Shannon-Fano_Compression_Algoritam.cpp
+++ Kompresija podataka bez gubitaka/Shannon-Fano_Compression_Algoritam.cpp
@@ -8,92 +8,115 @@ using namespace std;
 struct node {
 
     // for storing symbol
-    string sym;
+    string karakter;
 
-    // for storing probability or frequency
-    float pro;
+    //Predstavlja ucestanost pojavljivanja simobla
+    float ucestanost;
     int arr[20];
+    //Sadrzi vrh steka
     int top;
-} p[20];
+} cvor[20]; //Niz cvorova koji sluzi za cuvanje karaktera i njihove ucestanosti
 
 typedef struct node node;
 
-// function to find shannon code
-void shannon(int l, int h, node p[])
+
+
+// Funkcija za pronalazenje shanon-ovog koda
+//low predstavlja donji indeks od koga se ide
+//high predstavlja gornju granicu indeksa do koje se ide
+// low i high ogranicavaju blok karaktera konkretne rekurzije
+void shannon(int low, int high, node shannon_cvor[])
 {
     float pack1 = 0, pack2 = 0, diff1 = 0, diff2 = 0;
     int i, d, k, j;
-    if ((l + 1) == h || l == h || l > h) {
-        if (l == h || l > h)
+    if ((low + 1) == high || low == high || low > high) {
+        if (low == high || low > high)
             return;
-        p[h].arr[++(p[h].top)] = 0;
-        p[l].arr[++(p[l].top)] = 1;
+        shannon_cvor[high].arr[++(shannon_cvor[high].top)] = 0; //assigns 0 to the symbol, if length is 2 to the highest probability symbol
+        shannon_cvor[low].arr[++(shannon_cvor[low].top)] = 1;//assigns 1 to the symbol if length is 2 to the lowest probability symbol
         return;
     }
     else {
-        for (i = l; i <= h - 1; i++)
-            pack1 = pack1 + p[i].pro;
-        pack2 = pack2 + p[h].pro;
-        diff1 = pack1 - pack2;
+        //Sum of all probabilities expect the last one i.e, the highest probability
+        for (i = low; i <= high - 1; i++)
+            pack1 = pack1 + shannon_cvor[i].ucestanost;
+
+        pack2 = pack2 + shannon_cvor[high].ucestanost;//Last Probability
+        diff1 = pack1 - pack2;//Difference between the Sum of all probabilities and last probability i.e, Alpha
+
+        //If difference is negative
         if (diff1 < 0)
             diff1 = diff1 * -1;
+
         j = 2;
-        while (j != h - l + 1) {
-            k = h - j;
+        //Loop to find the number of bits required
+        while (j != high - low + 1) {
+            k = high - j;
             pack1 = pack2 = 0;
-            for (i = l; i <= k; i++)
-                pack1 = pack1 + p[i].pro;
-            for (i = h; i > k; i--)
-                pack2 = pack2 + p[i].pro;
+            //Sum of all probabilities
+            for (i = low; i <= k; i++)
+                pack1 = pack1 + shannon_cvor[i].ucestanost;
+
+            for (i = high; i > k; i--)
+                pack2 = pack2 + shannon_cvor[i].ucestanost;
+
             diff2 = pack1 - pack2;
+
+            //If difference is negative
             if (diff2 < 0)
-                diff2 = diff2 * -1;
+                diff2 = diff2 * -1; //Make it positive  to compare with the previous difference
+
+            //If difference is greater than previous difference
             if (diff2 >= diff1)
                 break;
-            diff1 = diff2;
-            j++;
+
+            diff1 = diff2;//Assign the difference to previous difference
+            j++; //Increment the number of bits
         }
         k++;
-        for (i = l; i <= k; i++)
-            p[i].arr[++(p[i].top)] = 1;
-        for (i = k + 1; i <= h; i++)
-            p[i].arr[++(p[i].top)] = 0;
+        //Assign 1 to the symbols with the lowest probability
+        for (i = low; i <= k; i++)
+            shannon_cvor[i].arr[++(shannon_cvor[i].top)] = 1;
+
+        //Assign 0 to the symbols with the highest probability
+        for (i = k + 1; i <= high; i++)
+            shannon_cvor[i].arr[++(shannon_cvor[i].top)] = 0;
 
         // Invoke shannon function
-        shannon(l, k, p);
-        shannon(k + 1, h, p);
+        shannon(low, k, shannon_cvor); //Recursive call to the function to assign the symbols with the lowest probability
+        shannon(k + 1, high, shannon_cvor);//Recursive call to the function to assign the symbols with the highest probability
     }
 }
 
-// Function to sort the symbols
-// based on their probability or frequency
-void sortByProbability(int n, node p[])
+//Funkcija sortira karaktere na osnovu njihove ucestanosti, u rastucem redosledu.
+void sortByProbability(int n, node cvor_za_sortiranje[])
 {
     int i, j;
-    node temp;
+    //Pomocni cvor koji pomaze pri sortiranju
+    node pom;
     for (j = 1; j <= n - 1; j++) {
         for (i = 0; i < n - 1; i++) {
-            if ((p[i].pro) > (p[i + 1].pro)) {
-                temp.pro = p[i].pro;
-                temp.sym = p[i].sym;
+            if ((cvor_za_sortiranje[i].ucestanost) > (cvor_za_sortiranje[i + 1].ucestanost)) {
+                pom.ucestanost = cvor_za_sortiranje[i].ucestanost;
+                pom.karakter = cvor_za_sortiranje[i].karakter;
 
-                p[i].pro = p[i + 1].pro;
-                p[i].sym = p[i + 1].sym;
+                cvor_za_sortiranje[i].ucestanost = cvor_za_sortiranje[i + 1].ucestanost;
+                cvor_za_sortiranje[i].karakter = cvor_za_sortiranje[i + 1].karakter;
 
-                p[i + 1].pro = temp.pro;
-                p[i + 1].sym = temp.sym;
+                cvor_za_sortiranje[i + 1].ucestanost = pom.ucestanost;
+                cvor_za_sortiranje[i + 1].karakter = pom.karakter;
             }
         }
     }
 }
 
-// function to display shannon codes
+// Funkcija za prikazivanje koda
 void display(int n, node p[])
 {
     int i, j;
-    cout << "\n\n\n\tSymbol\tProbability\tCode";
+    cout << "\n\n\n\tSymbol\tUcestanost\tCode";
     for (i = n - 1; i >= 0; i--) {
-        cout << "\n\t" << p[i].sym << "\t\t" << p[i].pro << "\t";
+        cout << "\n\t" << p[i].karakter << "\t\t" << p[i].ucestanost << "\t";
         for (j = 0; j <= p[i].top; j++)
             cout << p[i].arr[j];
     }
@@ -119,40 +142,40 @@ int main()
         cout << ch << endl;
 
         // Insert the symbol to node
-        p[i].sym += ch;
+        cvor[i].karakter += ch;
     }
 
     // Input probability of symbols
     float x[] = { 0.22, 0.28, 0.15, 0.30, 0.05 };
     for (i = 0; i < n; i++) {
-        cout << "\nEnter probability of " << p[i].sym << " : ";
+        cout << "\nEnter probability of " << cvor[i].karakter << " : ";
         cout << x[i] << endl;
 
         // Insert the value to node
-        p[i].pro = x[i];
-        total = total + p[i].pro;
+        cvor[i].ucestanost = x[i];
+        total = total + cvor[i].ucestanost;
 
         // checking max probability
         if (total > 1) {
             cout << "Invalid. Enter new values";
-            total = total - p[i].pro;
+            total = total - cvor[i].ucestanost;
             i--;
         }
     }
 
-    p[i].pro = 1 - total;
+    cvor[i].ucestanost = 1 - total;
 
     // Sorting the symbols based on
     // their probability or frequency
-    sortByProbability(n, p);
+    sortByProbability(n, cvor);
 
     for (i = 0; i < n; i++)
-        p[i].top = -1;
+        cvor[i].top = -1;
 
     // Find the shannon code
-    shannon(0, n - 1, p);
+    shannon(0, n - 1, cvor);
 
     // Display the codes
-    display(n, p);
+    display(n, cvor);
     return 0;
 }
